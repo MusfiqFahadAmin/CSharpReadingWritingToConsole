1033b9c35ac0a3b55b204b381d664359269a7329
Musfiq Fahad Amin <musfiqfahadamin@gmail.com>
2020-08-30 19:13:28 +0600

    Add project files.

---
+++ CSharpReadingWritingToConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpReadingWritingToConsole
+{
+    class Program
+    {
+        static void Main ( string [ ] args )
+        {
+            Console . WriteLine ( "Please Enter Your First Name: " );
+            /* Reads User Input from the Console Using ReadLine()
+             * Method and then Storing the Value in string*/
+            string firstName = Console.ReadLine();
+
+            Console . WriteLine ( "Please Enter Your Last Name: " );
+            string lastName = Console.ReadLine();
+
+            Console . WriteLine ( "Hello, {0} {1}!" , firstName , lastName );
+        }
+    }
+}
